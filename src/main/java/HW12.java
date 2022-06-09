6032889b2f333f450d849a530c37aa4ba3e0fb35
artemmer <art.meriuts@gmail.com>
2022-06-09 17:23:14 +0200

    Initial commit

---
+++ src/main/java/HW12.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class HW12 {
+
+    public static void main(String[] args) {
+        int[] zero = {1,5,33,0,44,52,0,47,68,99,0};
+        Arrays.sort(zero);
+        System.out.println(Arrays.toString(zero));
+    }
+
+
+
+}
